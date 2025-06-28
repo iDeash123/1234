98c039224fbc27fa964d0e8eeadc853a1707fefc
iDeash123 <istresnaa@gmail.com>
2025-06-28 13:57:35 +0300

    new_css

--- heavyaura/main/views.py
+++ heavyaura/main/views.py
@@ -6,7 +6,7 @@ from cart.forms import CartAddProductForm
 
 
 def popular_list(request):
-    products = Product.objects.filter(available=True)[:6]
+    products = Product.objects.filter(available=True).order_by('?')[:5]
     return render(request, 
                   'main/index/index.html', 
                   {'products': products})
